22b8fa3cf3bafe90890ff3215537f501e2e5932c
Robson <bynho92k@gmail.com>
2023-10-07 11:40:34 -0300

    api added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
-require('dotenv').config()
+const apiKey = "6c67002aaeec83abb358aba2fc8a15ef";
+const apiCountryURL = "https://flagsapi.com/";
 
-const apiKey = process.env.ACCESS;
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const countryElement = document.querySelector("country");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+// Funções
+
+const getWeatherData = async (city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  console.log(data);
+};
+
+const showWeatherData = (city) => {
+  getWeatherData(city);
+};
+
+// EVENTOS
+
+searchBtn.addEventListener("click", (Element) => {
+  Element.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
